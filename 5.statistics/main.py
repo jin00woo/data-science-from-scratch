0a1eb8010ea5a5653da9fb6aba1a709dc99d385c
Jinwoo Han <56533546+jin00woo@users.noreply.github.com>
2022-07-22 07:41:43 +0000

    Finished writing code for chapter 5.

--- 5.statistics/main.py
+++ 5.statistics/main.py
@@ -54,3 +54,47 @@ def mode(data: List[float]) -> float:
 
 # print(mode(num_friends))
 
+# Range
+def data_range(data: List[float]) -> float:
+  return max(data) - min(data)
+
+def interquartile_range(data: List[float]) -> float:
+  return quantile(data, 0.75) - quantile(data, 0.25)
+
+# Variance
+def de_mean(data: List[float]) -> List[float]:
+  x_bar = mean(data)
+  return [x - x_bar for x in data]
+
+def sum_of_squares(data: List[float]) -> float:
+  return sum(element**2 for element in data)
+
+def variance(data: List[float]) -> float:
+  assert len(data) >= 2, "at least two elements required"
+  n = len(data)
+  deviations = de_mean(data)
+  return sum_of_squares(deviations) / (n-1)
+
+# Standard deviation
+import math
+
+def standard_deviation(data: List[float]) -> float:
+  return math.sqrt(variance(data))
+
+# Covariance
+def dot(x: List[float], y: List[float]) -> float:
+  return sum(x_i * y_i for x_i, y_i in zip(x,y))
+
+def covariance(x: List[float], y: List[float]) -> float:
+  assert len(x) == len(y), "Two data sets must have same number of elements"
+
+  return dot(de_mean(x), de_mean(y)) / (len(x) - 1)
+
+# Correlation
+def correlation(x: List[float], y: List[float]) -> float:
+  stdev_x = standard_deviation(x)
+  stdev_y = standard_deviation(y)
+  if stdev_x > 0 and stdev_y > 0:
+    return covariance(x,y) / stdev_x / stdev_y
+  else:
+    return 0
